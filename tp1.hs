1df10e742924936464b7c4783d0bd67f84ec5c9d
ignaciano3 <igarciap@fi.uba.ar>
2021-09-15 19:32:48 -0300

    Update tp1.hs

--- tp1.hs
+++ tp1.hs
@@ -1,13 +1,26 @@
-
-c x = round(x ** (1/3))
-
 -- Ejercicio 1
 esSumaDeDosCubos :: Integer -> Bool
 esSumaDeDosCubos x = True 
 
 -- Ejercicio 2
-descomposicionCubos :: Integer -> (Integer,Integer)
-descomposicionCubos x = (1,1)
+-- x = a^3+b^3
+-- sol = (a, sqrt3(x-b^3))
+
+a :: Integer
+a = 2
+
+b :: Integer
+b = 1
+
+descomposicionCubos :: Integer -> (Integer, Integer)
+descomposicionCubos x | a^3 + b^3 == x = (a,b)
+                      | a^3 > x = (a-1, b)
+                      | b^3 > x = (a, b-1)
+                      | otherwise = (0,0)
+--descomposicionCubos x = (a, floor ((x - fromIntegral a ^ 3) ** (1/3))) 
+
+
+
 
 -- Ejercicio 3
 cantidadDeFormas :: Integer -> Integer
@@ -24,7 +37,3 @@ especialNumero x = x
 -- Ejercicio 6
 esMuyEspecial :: Integer -> Bool
 esMuyEspecial x = True
-
-
-
-
