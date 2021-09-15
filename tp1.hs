ad3a16f9f11a1efc0586a2338dbf9e480f434a73
ignaciano3 <igarciap@fi.uba.ar>
2021-09-15 15:54:25 -0300

    Update tp1.hs

--- tp1.hs
+++ tp1.hs
@@ -1,3 +1,27 @@
--- Funcion 1
+-- Ejercicio 1
 esSumaDeDosCubos :: Integer -> Bool
 esSumaDeDosCubos x = True 
+
+-- Ejercicio 2
+descomposicionCubos :: Integer -> (Integer,Integer)
+descomposicionCubos x = (1,1)
+
+-- Ejercicio 3
+cantidadDeFormas :: Integer -> Integer
+cantidadDeFormas x = 1
+
+-- Ejercicio 4
+especialDesde :: Integer -> Integer
+especialDesde x = x
+
+-- Ejercicio 5
+especialNumero :: Integer -> Integer
+especialNumero x = x
+
+-- Ejercicio 6
+esMuyEspecial :: Integer -> Bool
+esMuyEspecial x = True
+
+
+
+
