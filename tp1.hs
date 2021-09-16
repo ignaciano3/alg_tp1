ad76936e44f0510a359e50471ea4758fd8003ac1
ignaciano3 <igarciap@fi.uba.ar>
2021-09-16 16:13:37 -0300

    Update tp1.hs

--- tp1.hs
+++ tp1.hs
@@ -5,36 +5,38 @@
 -- and output the pair otherwise, stopping when x and y cross. 
 -- If you only want to know whether or not such a pair exists, you can stop as soon as you find one.
 
-
-
 -- Ejercicio 1
 esSumaDeDosCubos :: Integer -> Bool
-esSumaDeDosCubos x = True 
+esSumaDeDosCubos x = verificarCubos x 0 (truncate (fromIntegral x**(1/3))) 
+
+verificarCubos x a b    | n == x = True
+                        | a > b = False
+                        | n < x = verificarCubos x (a+1) b
+                        | n > x = verificarCubos x a (b-1)
+                        | otherwise = False
+                            where n = a^3 + b^3
 
 -- Ejercicio 2
-
-a = 0
-
-f x = truncate (fromIntegral x**(1/3)) 
-
-b :: Integer
-b = 1
-
--- No se como iterar el a y el b
 descomposicionCubos :: Integer -> (Integer, Integer)
-descomposicionCubos x | a^3 + b^3 == x = (a,b)
-                      | a^3 > x = (a-1, b)
-                      | b^3 > x = (a, b-1)
-                      | otherwise = (0,0)
---descomposicionCubos x = (a, floor ((x - fromIntegral a ^ 3) ** (1/3))) 
+descomposicionCubos x = buscarCubos x 0 (truncate (fromIntegral x**(1/3)))
 
-
-
+buscarCubos x a b   | n == x = (a, b)
+                    | a > b = (0,0) -- Si no encuentra devuelve (0,0)
+                    | n < x = buscarCubos x (a+1) b
+                    | n > x = buscarCubos x a (b-1)
+                    | otherwise = (0,0)
+                        where n = a^3 + b^3                    
 
 -- Ejercicio 3
 cantidadDeFormas :: Integer -> Integer
-cantidadDeFormas x = 1
+cantidadDeFormas x = buscarCantidad x 0 (truncate (fromIntegral x**(1/3)))
 
+buscarCantidad x a b    | a > b = 0
+                        | n == x = 1 + buscarCantidad x (a+1) b
+                        | n < x = buscarCantidad x (a+1) b
+                        | n > x = buscarCantidad x a (b-1)
+                        | otherwise = 0
+                            where n = a^3 + b^3
 -- Ejercicio 4
 especialDesde :: Integer -> Integer
 especialDesde x = x
