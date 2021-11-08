143fcb4af867c7fb8c57c1a0dfe7bc8e41867767
BocciaJuan <90794851+BocciaJuan@users.noreply.github.com>
2021-11-08 11:44:02 -0300

    Update Aritmetica.hs

--- Aritmetica.hs
+++ Aritmetica.hs
@@ -4,13 +4,8 @@ import Data.Tuple
 import Data.Bits
 import Data.List
 
---(1)
--- Ignorar esto y si es posible eliminarlo que no sirve para naaa
-mcdExt :: Integer -> Integer -> (Integer, (Integer, Integer))
-mcdExt _ _ = (0, (0, 0))
+--(1)-----------------------------------------------------------------------------
 
---(2)-----------------------------------------------------------------------------
--- Every prime number can be represented in form of 6n+1 or 6n-1 except the prime number 2 and 3, where n is a natural number.
 -- No obliga a hacer el algortmo criba así que hice uno mejor
 criba :: Integer -> Set Integer
 criba num = encontrarPrimos num 1 [2, 3]
@@ -33,7 +28,7 @@ esPrimo n = verificarEsPrimo (fromIntegral n) 2
 
 verificarEsPrimo :: Integral a => a -> a -> Bool
 verificarEsPrimo n i  | n == 1 = False -- 1 no es primo
-                      | n == 3 = True
+                      | n == 3 = True -- 3 es Primo
                       | n `mod` i == 0 = False -- es compuesto
                       | i > fromIntegral sqrtn = True -- verificar más números no sirve
                       | otherwise = verificarEsPrimo n (i+1) -- proximo i
@@ -42,13 +37,7 @@ verificarEsPrimo n i  | n == 1 = False -- 1 no es primo
 -- criba de 10,000 sin i > sqrtn tarda 17.14 segundos :-/ me gustaria que tarde 5 como mucho
 -- con i > sqrtn tarda 1.3 segundos aprox :-D ahi anda bien
 
---(3)-----------------------------------------------------------------------------
---Example 1. Let the two numbers be 10 and 90.
--- We have,
--- 45 = 3×3×5 
--- 90 = 2×3×3×5
--- Common factors = 3,3 and 5
--- Therefore, gcd(45,90)=3×3×5=45≠1⇒45 and 90 are not coprime numbers.
+--(2)-----------------------------------------------------------------------------
 
 -- Entoces para sacar coprimos de n buscamos primos que no sean factor comun a n
 -- Para eso hago una funcion que sea sacar factores comunes
@@ -68,10 +57,12 @@ encontrarCoprimo num set i it | num == 2 = 1 -- coprimo con 2
                               | i `mod` (set !! it) /= 0 = encontrarCoprimo num set i (it+1) -- si no es compuesto con uno verifico con el resto del set
                               | otherwise = encontrarCoprimo num set (i+1) 0 -- proximo i
 
---(4)
+--()
 inversoMultiplicativo:: Integer -> Integer -> Integer
 inversoMultiplicativo num1 num2 | not (sonCoprimos num1 num2) = -99
-                                | otherwise = euclidesExtendido num2 num1 1 0 0 1 num2 -- num2 es por si queda negativo
+                                | a < 0 = a + num2
+                                | a > 0 = a
+                                  where euclidesExtendido num2 num1 1 0 0 1 = a
 
 -- Verifico si son coprimos
 sonCoprimos num1 num2 = boolFactoresDistintos (factores num1) (factores num2) 0 0
@@ -84,17 +75,14 @@ boolFactoresDistintos set1 set2 it1 it2 | (set1 !! it1) == (set2 !! it2) = False
 -- Aplico el algoritmo de Euclides Extendido
 -- https://www.youtube.com/watch?v=D289EF58Yrw
 euclidesExtendido :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer
-euclidesExtendido g0 g1 u0 u1 v0 v1 num2 | g1 /= 0 = euclidesExtendido g1 g2 u1 u2 v1 v2 num2
-                                       --                              g0 g1 u0 u1 v0 v1 B queda igual
-                                      | g1 == 0 && v0 < 0 = v0 + num2 -- se pudiera poner a num2 afuera de la funcion 
-                                      | otherwise = v0
+euclidesExtendido g0 g1 u0 u1 v0 v1 | g1 /= 0 = euclidesExtendido g1 g2 u1 u2 v1 v2
+                                       --                              g0 g1 u0 u1 v0 v1 queda igual
+                                    | g1 == 0 = v0 
                                       where 
                                         y2 = g0 `div` g1 --mirar el video
                                         g2 = g0 - y2 * g1
                                         u2 = u0 - y2 * u1
                                         v2 = v0 - y2 * v1
-
--- Metete la función de regalo por el *rt*
 
 -- Función de regalo para exponenciar "rápido"
 modExp :: Integer -> Integer -> Integer -> Integer -- si saben que hace diganme x que no se para que sirve
